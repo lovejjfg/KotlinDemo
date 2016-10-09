3bffcfc7559b952b53b6a3dff5710813e8fb19fb
zhangjun <zhangjun@ehousechina.com>
2016-10-09 15:06:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tt/kotlindemo/Main2Activity.kt
@@ -0,0 +1,48 @@
+package com.example.tt.kotlindemo
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.view.View
+import java.util.*
+
+class Main2Activity : AppCompatActivity() {
+    internal var listener: View.OnClickListener? = null
+    var list: ArrayList<String>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        setClickListener(View.OnClickListener { view ->
+            val i = Intent(this@Main2Activity, Main4Activity::class.java)
+            startActivity(i)
+        })
+        list = ArrayList<String>()
+        list!!.add("1")
+        list!!.add("2")
+        list!!.add("3")
+        list!!.add("4")
+
+        for (i in list!!.asIterable()) {
+            Log.i(i, i)
+        }
+
+        for (j in 0..6) {
+            Log.e("j", "" + j)
+        }
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+//        fab.setOnClickListener({ view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() })
+        fab.setOnClickListener(listener)
+    }
+
+    fun setClickListener(listener: View.OnClickListener) {
+        this.listener = listener
+    }
+
+}
---
+++ app/src/main/java/com/example/tt/kotlindemo/Main4Activity.kt
@@ -0,0 +1,26 @@
+package com.example.tt.kotlindemo
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+
+class Main4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener {
+            val i = Intent(this@Main4Activity, Main2Activity::class.java)
+            startActivity(i)
+        }
+    }
+
+}
